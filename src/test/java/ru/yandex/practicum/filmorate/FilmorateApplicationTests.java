2ed14b09e7b649a4b5067c28da16af8eb38be572
vladbakumenko <klaid-slaid@yandex.ru>
2022-09-26 15:34:10 +0400

    message

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -17,6 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
+sdfsdfsdfsdfsdfsdfsdfsdfgit
 class FilmorateApplicationTests {
 
     FilmController filmController;

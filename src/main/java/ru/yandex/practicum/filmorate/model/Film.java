766817c8d82fc0f623a3883f3cc4aad4ddb25c03
vladbakumenko <klaid-slaid@yandex.ru>
2022-10-11 17:39:07 +0400

    m2

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,35 +1,20 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
-import org.springframework.lang.NonNull;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
-import java.time.Duration;
 import java.time.LocalDate;
 
 @Data
 public class Film {
     private int id;
-    @NotNull @NotBlank @NotEmpty
+    @NotBlank
     private String name;
     private String description;
     private LocalDate releaseDate;
     @Positive
     private int duration;
-
-    public Film() {
-
-    }
-
-//    public Film(String name, String description, LocalDate releaseDate, int duration) {
-//        this.name = name;
-//        this.description = description;
-//        this.releaseDate = releaseDate;
-//        this.duration = duration;
-//    }
 
     public Film(int id, String name, String description, LocalDate releaseDate, int duration) {
         this.id = id;
